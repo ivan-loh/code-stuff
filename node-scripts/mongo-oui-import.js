af4178ee4ab544f79d67e5c3d28cf1ce8c52b13f
Ivan Loh <ivan@eriad.com>
2013-12-11 13:59:21 +0800

    add in mongo-oui-import script

---
+++ node-scripts/mongo-oui-import.js
@@ -0,0 +1,91 @@
+var MongoClient = require('mongodb').MongoClient;
+var Server      = require('mongodb').Server;
+var url         = require('url');
+var filename    = __dirname + '/' + require('node-uuid').v4();
+var lineReader  = require('line-reader');
+var client      = new MongoClient(new Server('localhost', 27017));
+
+
+/*
+ * File Download
+ */
+var download = function download(next, client) {
+
+  // Http Request options
+  var options = url.parse('http://standards.ieee.org/develope/regauth/oui/oui.txt');
+  options.agent = false;
+
+  // Download handler
+  var handle = function handle(res) {
+    var outFile = fs.createWriteStream(filename);
+    res.setEndocding('utf8');
+    res.on('data', function (chunk) {
+	outFile.write(chunk);
+     }).on('end', function () {
+	outFile.end();
+	return next(null, client);
+     }).on('close', function () {
+	outFile.end();
+	fs.unlink(filename, function () {
+	  return next(new Error('premature eof'));
+	});
+     });
+  };
+
+  // error handler
+  var error = function error(err) {
+    return next(err);
+  };
+
+  // Request!
+  http.request(options, handle).on('error',error).end();
+
+};
+
+/*
+ * Database Insert
+ */
+var insert = function insert(err, client) {
+
+  var db      = client.db('tide');
+  var oui     = db.collection('oui');
+  var options = { w:1, continueOnError: true };
+
+  var display = function (err, count) {
+    if (err) console.warn(err);
+  };
+
+  // File Reading.
+  lineReader.eachLine(filename, function (line) {
+    var oui, name;
+    line = line.trimLeft().trimRight();
+
+    // Extraction
+    if (line.length > 15) {
+      oui  = line.substr(0,8).split('-').join('');
+      line = line.substr(9).trimLeft();
+      if (line.substr(0,5) === '(hex)')
+	name = line.substr(6).trimLeft();
+    }
+
+    // Insertion
+    if ( (!!oui)  && (oui.length === 6) &&
+	 (!!name) && (name.length > 0) ) {
+      oui({ _id: oui, name: name }, options, display);
+    }
+
+  }).then(function () {
+    // Clean Up
+    fs.unlink(filename, function(err) {
+      if (err) console.warn(err);
+      db.close();
+      console.log('Import Completed');
+    });
+  });
+
+};
+
+// Let the games begin.
+client.open(function (err, client) {
+  download(insert, client);
+});
