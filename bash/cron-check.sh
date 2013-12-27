c757374dfb64af30e4c6f2655738fd0c43bbd187
Ivan <ivan@eriad.com>
2013-12-27 14:25:39 +0800

    added sudo

--- bash/cron-check.sh
+++ bash/cron-check.sh
@@ -1,1 +1,8 @@
-for user in $(cut -f1 -d: /etc/passwd); do echo $user; crontab -u $user -l; done
+#!/bin/bash
+
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+for user in $(cut -f1 -d: /etc/passwd); do echo $user; sudo crontab -u $user -l; done
